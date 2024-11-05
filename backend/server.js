b222494dd82b2473bd3ce6033f76827e60c235b9
adama diouf <adamadiouf2017@gmail.com>
2024-11-05 12:56:37 -0600

    add dirname

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use('/api/form/contact', contactRoute)
 
-
+const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')))
   app.get('*', (req, res) =>
